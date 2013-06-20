25127f5fdf0a9bf289a1bcebf017bfa253667cb0
pjarnfelt <pjarnfelt@gmail.com>
2013-06-20 17:03:18 +0200

    license update

--- src/UniMoveController.cs
+++ src/UniMoveController.cs
@@ -1,37 +1,42 @@
  /**
  * UniMove API - A Unity plugin for the PlayStation Move motion controller
- * Copyright (C) 2011, Copenhagen Game Collective (http://www.cphgc.org)
- * 					   Patrick Jarnfelt
- * 					   Douglas Wilson (http://www.doougle.net)
+ * Copyright (C) 2012, 2013, Copenhagen Game Collective (http://www.cphgc.org)
+ * 					         Patrick Jarnfelt
+ * 					         Douglas Wilson (http://www.doougle.net)
  * 
- * Version 0.1.0 (Beta)
- * 2011-07-19
  * 
- * Email us at: code@cphgc.org
+ * All rights reserved.
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
  *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ *    1. Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the
- * Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *    2. Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  **/
 
  /**
  * IMPORTANT NOTES!
  *
- * -- This API has been compiled for Mac OSX (10.5 and 10.6) specifically. 
+ * -- This API has been compiled for Mac OSX (10.7 and later) specifically.
  * 
  * -- This API assumes that the controller has already been paired and connected by Bluetooth beforehand.
- *    To pair a controller(s), we use our own Pairing Utility (included).
+ *    To pair a controller(s), use the Pairing Utility provided by the PS Move API http://thp.io/2010/psmove/.
  *    To connect a controller by Bluetooth, just press the PS button after pairing it.
  *    You can also use the controllers by USB, but with limited functionality (see below).
  * 
@@ -40,9 +45,12 @@
  * 	- Setting the RGB LED color and rumble intensity (USB and Bluetooth)
  * 	- Read the status of the digital buttons (Bluetooth only)
  * 	- Read the status of the analog trigger (Bluetooth only)
- * 	- Read sensor values for the accelerometer and gyroscope (Bluetooth only)
- * 
- * -- In the near future, we hope to add hooks to read the temperature and battery values.
+ * 	- Read values for the internal sensors (Bluetooth only):
+ *     - accelorometer
+ *     - gyroscope
+ *     - magnetometer
+ *     - temperature
+ *     - battery level
  * 
  * Please see the README for more information!
  **/
